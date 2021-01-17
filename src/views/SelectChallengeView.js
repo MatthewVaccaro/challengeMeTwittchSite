72a11bf023d90857e7e6d5c52acef74eabcc72ef
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-01-17 16:04:39 -0600

    ensure users going directly to the url couldn't submit a challenge while the streamer was offline

--- src/views/SelectChallengeView.js
+++ src/views/SelectChallengeView.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { EntriesContext } from '../global/entriesContext';
+import { StreamContext } from '../global/streamContext';
 import { useHistory } from 'react-router-dom';
 import { motion } from 'framer-motion';
 //Request
@@ -18,6 +19,7 @@ import Button from '../utils/Button';
 import Notification from '../baseComponents/notification';
 const SelectChallengeView = (props) => {
 	const [ entries, setEntries ] = useContext(EntriesContext);
+	const [ stream, setStream ] = useContext(StreamContext);
 	const gameID = props.match.params.id;
 	const username = props.match.params.username;
 
@@ -31,6 +33,8 @@ const SelectChallengeView = (props) => {
 		challenge_id_fk: '',
 		customChallenge: ''
 	});
+
+	console.log('steammmmmmm', stream);
 
 	const [ notification, setNotification ] = useState();
 	const [ type, setType ] = useState('meme');
@@ -55,158 +59,173 @@ const SelectChallengeView = (props) => {
 	);
 
 	return (
-		<div className="container px-3 mx-auto sm:max-w-5xl">
-			{notification ? notification === 'success' || notification === 'upvote' ? (
-				<motion.div
-					className="absolute z-20 w-full left-0"
-					initial="start"
-					animate="end"
-					variants={position}
-					transition={{ ease: 'easeIn', duration: 0.5 }}
-				>
-					<Notification status={notification === 'upvote' ? 'upvote' : true} />
-				</motion.div>
-			) : (
-				<motion.div
-					className="absolute z-20 w-full left-0"
-					initial="start"
-					animate="end"
-					variants={position}
-					transition={{ ease: 'easeIn', duration: 0.5 }}
-				>
-					<Notification status={false} />
-				</motion.div>
-			) : (
-				''
-			)}
-
-			<Back />
-			<h1 className="h1-dark mt-2 mb-8 ">{challenges ? challenges[0].title : ''} Challenges</h1>
-			<Input
-				name={'challenger'}
-				placeholder={'Enter Your name'}
-				label={'Initiator’s Name'}
-				value={select.challenger}
-				setState={setSelect}
-				state={select}
-				type={'text'}
-			/>
-
-			<TypeTab state={type} setState={setType} />
-			<p className="p-mid font-semibold">
-				<span className={`text-${findColor(type)[1]} capitalize`}>{type}</span> Challenges
-			</p>
-			{type === 'custom' ? (
-				<Input
-					name={'customChallenge'}
-					placeholder={'Enter Custom Challenge Here'}
-					label={''}
-					value={select.customChallenge}
-					setState={setSelect}
-					state={select}
-					type={'text'}
-				/>
-			) : challenges ? (
-				challenges.map((cv) => {
-					if (cv.type === type) {
-						return (
-							<motion.div>
-								<Card
-									id={cv.id}
-									header={cv.content}
-									leftElement={<Radio obj={cv} state={select} setState={setSelect} key={cv.id} />}
-								/>
-							</motion.div>
-						);
-					}
-				})
-			) : (
-				''
-			)}
-			<motion.div
-				className="absolute bottom-0"
-				initial="start"
-				animate={
-					(select.challenger && select.challenge_id_fk) || (select.challenger && select.customChallenge) ? (
-						'end'
+		<div>
+			{stream ? (
+				<div className="container px-3 mx-auto sm:max-w-5xl">
+					{notification ? notification === 'success' || notification === 'upvote' ? (
+						<motion.div
+							className="absolute z-20 w-full left-0"
+							initial="start"
+							animate="end"
+							variants={position}
+							transition={{ ease: 'easeIn', duration: 0.5 }}
+						>
+							<Notification status={notification === 'upvote' ? 'upvote' : true} />
+						</motion.div>
+					) : (
+						<motion.div
+							className="absolute z-20 w-full left-0"
+							initial="start"
+							animate="end"
+							variants={position}
+							transition={{ ease: 'easeIn', duration: 0.5 }}
+						>
+							<Notification status={false} />
+						</motion.div>
 					) : (
 						''
-					)
-				}
-				variants={buttonPop}
-				transition={{ type: 'spring', damping: 10, duration: 0.3 }}
-				onClick={() => {
-					if (select.challenger && select.customChallenge) {
-						return POST_customChallenge(gameID, {
-							type: 'custom',
-							content: select.customChallenge
+					)}
+
+					<Back />
+					<h1 className="h1-dark mt-2 mb-8 ">{challenges ? challenges[0].title : ''} Challenges</h1>
+					<Input
+						name={'challenger'}
+						placeholder={'Enter Your name'}
+						label={'Initiator’s Name'}
+						value={select.challenger}
+						setState={setSelect}
+						state={select}
+						type={'text'}
+					/>
+
+					<TypeTab state={type} setState={setType} />
+					<p className="p-mid font-semibold">
+						<span className={`text-${findColor(type)[1]} capitalize`}>{type}</span> Challenges
+					</p>
+					{type === 'custom' ? (
+						<Input
+							name={'customChallenge'}
+							placeholder={'Enter Custom Challenge Here'}
+							label={''}
+							value={select.customChallenge}
+							setState={setSelect}
+							state={select}
+							type={'text'}
+						/>
+					) : challenges ? (
+						challenges.map((cv) => {
+							if (cv.type === type) {
+								return (
+									<motion.div>
+										<Card
+											id={cv.id}
+											header={cv.content}
+											leftElement={
+												<Radio obj={cv} state={select} setState={setSelect} key={cv.id} />
+											}
+										/>
+									</motion.div>
+								);
+							}
 						})
-							.then((res) => {
-								POST_entry(gameID, { challenge_id_fk: res.data[0].id, challenger: select.challenger })
-									.then(() => {
-										setNotification('success');
-										setTimeout(() => {
-											history.push(`/${username}`);
-										}, 3000);
+					) : (
+						''
+					)}
+					<motion.div
+						className="absolute bottom-0"
+						initial="start"
+						animate={
+							(select.challenger && select.challenge_id_fk) ||
+							(select.challenger && select.customChallenge) ? (
+								'end'
+							) : (
+								''
+							)
+						}
+						variants={buttonPop}
+						transition={{ type: 'spring', damping: 10, duration: 0.3 }}
+						onClick={() => {
+							if (select.challenger && select.customChallenge) {
+								return POST_customChallenge(gameID, {
+									type: 'custom',
+									content: select.customChallenge
+								})
+									.then((res) => {
+										POST_entry(gameID, {
+											challenge_id_fk: res.data[0].id,
+											challenger: select.challenger
+										})
+											.then(() => {
+												setNotification('success');
+												setTimeout(() => {
+													history.push(`/${username}`);
+												}, 3000);
+											})
+											.catch((error) => {
+												setNotification('error');
+												console.log(error);
+												setTimeout(() => {
+													setNotification('');
+												}, 3000);
+											});
 									})
-									.catch((error) => {
+									.catch((err) => {
 										setNotification('error');
-										console.log(error);
+										console.log(err);
 										setTimeout(() => {
 											setNotification('');
 										}, 3000);
 									});
-							})
-							.catch((err) => {
-								setNotification('error');
-								console.log(err);
-								setTimeout(() => {
-									setNotification('');
-								}, 3000);
-							});
-					}
-					else {
-						const findDuplicate = entries.filter((entry) => {
-							return entry.content === select.content;
-						});
+							}
+							else {
+								const findDuplicate = entries.filter((entry) => {
+									return entry.content === select.content;
+								});
 
-						if (findDuplicate) {
-							return PUT_upvote(findDuplicate[0].id, { vote: 'plus' })
-								.then(() => {
-									setNotification('upvote');
-									setTimeout(() => {
-										history.push(`/${username}`);
-									}, 3000);
-								})
-								.catch((error) => {
-									setNotification('error');
-									console.log(error);
-									setTimeout(() => {
-										setNotification('');
-									}, 3000);
-								});
-						}
-						else {
-							return POST_entry(gameID, select)
-								.then(() => {
-									setNotification('success');
-									setTimeout(() => {
-										history.push(`/${username}`);
-									}, 3000);
-								})
-								.catch((error) => {
-									setNotification('error');
-									console.log(error);
-									setTimeout(() => {
-										setNotification('');
-									}, 3000);
-								});
-						}
-					}
-				}}
-			>
-				<Button text={'Send Challenge'} bg={'blue'} icon={'challenge'} />
-			</motion.div>
+								if (findDuplicate) {
+									return PUT_upvote(findDuplicate[0].id, { vote: 'plus' })
+										.then(() => {
+											setNotification('upvote');
+											setTimeout(() => {
+												history.push(`/${username}`);
+											}, 3000);
+										})
+										.catch((error) => {
+											setNotification('error');
+											console.log(error);
+											setTimeout(() => {
+												setNotification('');
+											}, 3000);
+										});
+								}
+								else {
+									return POST_entry(gameID, select)
+										.then(() => {
+											setNotification('success');
+											setTimeout(() => {
+												history.push(`/${username}`);
+											}, 3000);
+										})
+										.catch((error) => {
+											setNotification('error');
+											console.log(error);
+											setTimeout(() => {
+												setNotification('');
+											}, 3000);
+										});
+								}
+							}
+						}}
+					>
+						<Button text={'Send Challenge'} bg={'blue'} icon={'challenge'} />
+					</motion.div>
+				</div>
+			) : (
+				<div className="container px-3 mx-auto sm:max-w-5xl">
+					<Back />
+					<h1 className="h1-dark mt-2 mb-8 "> User Isn't currently taking challenges. </h1>
+				</div>
+			)}
 		</div>
 	);
 };
